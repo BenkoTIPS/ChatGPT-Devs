d70bc226da7cbc7459893b33ec74ca2b4d940d81
mbenko <mbenko@benko.com>
2023-09-16 12:07:15 -0500

    Cleanup setup

--- buildit.ps1
+++ buildit.ps1
@@ -1,3 +1,4 @@
+# ChatGPT Devs - Buildit
 git init
 
 dotnet new razor -o code/myChat
@@ -10,3 +11,13 @@ git remote add origin https://github.com/BenkoTIPS/ChatGPT-Devs.git
 git branch -M main
 git push -u origin main
 
+## Demos
+
+
+
+dotnet user-secrets -p code/myChat init
+dotnet user-secrets -p code/myChat set "OpenAI:ApiKey" "your-api-key-here"
+
+dotnet add code/myChat package Azure.AI.OpenAI --version 1.0.0-beta.5
+
+start powerpnt.exe .\docs\bnk-openai-dev.pptx
